897d3a2f56812c5d01007c104fc470cde4383f29
Jonghyun <wdh008@naver.com>
2020-06-03 19:12:20 +0900

    MainPage finish

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,26 +2,50 @@ import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import MainImage from './Sections/MainImage';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 function LandingPage() {
 
     const [Movies, setMovies] = useState([])
     const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [CurrentPage, setCurrentPage] = useState(0)
 
     useEffect(() => {
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-
         fetch(endpoint)
-        .then(response => response.json())
-        .then(response => {
-
-            setMovies([response.results])
-            setMainMovieImage(response.results[0])
-        })
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+                setMainMovieImage(response.results[0])
+            })
+        //위에 fetch 함수 부분을 지우고 fetchMovies 함수의 주석을 없애고 
+        //fetchMovies 함수에서 setMainMovieImage도 설정을 해주면 
+        //morebutton을 클릭할 때마다 메인 이미지도 같이 변경 
+        //현제는 mainimage는 고정해 놓은 상태 
+        fetchMovies(endpoint)
 
 
     }, [])
 
+    const fetchMovies = (endpoint) => {
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+                setMovies([...Movies, ...response.results])
+                //setMainMovieImage(response.results[0])
+                setCurrentPage(response.page)
+            })
+    }
+
+
+    const loadMoreItems = () => {
+
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`;
+        fetchMovies(endpoint)
+
+    }
 
     return (
         <div style={{ width: '100%', margin: '0' }}>
@@ -40,12 +64,27 @@ function LandingPage() {
                 <hr />
 
                 {/** Movie Grid Cards */}
+                <Row gutter={[16, 16]} >
+
+                {Movies && Movies.map((movie, index) => (
+                    <React.Fragment key={index}>
+                        <GridCards  
+                        image={movie.poster_path ?
+                            `${IMAGE_BASE_URL}w500${movie.poster_path}` : null} 
+                        movieId={movie.id}
+                        movieName={movie.original_title}
+                        />
+
+                    </React.Fragment>
+                ))}
+
+                </Row>
 
 
             </div>
 
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <button> Load More </button>
+                <button onClick={loadMoreItems}> Load More </button>
             </div>
         </div>
     )
