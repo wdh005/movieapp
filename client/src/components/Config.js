8817500171e4d86bd84ae1359b6961bdf4be5790
Jonghyun <wdh008@naver.com>
2020-06-03 18:10:16 +0900

    Start Coding

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '80c52bb1fcf6696741a8919a03e6a08c';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
