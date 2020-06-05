0fa22d00adce175b210c4d47a107be8b6478cb71
Jonghyun <wdh008@naver.com>
2020-06-05 17:19:10 +0900

    comment & reply & likeDislike 기능 추가 최종 완성

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -4,3 +4,9 @@ export const USER_SERVER = '/api/users';
 export const API_URL = 'https://api.themoviedb.org/3/';
 export const API_KEY = '80c52bb1fcf6696741a8919a03e6a08c';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+//Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
+
+// w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE = 'w500'
--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -1,95 +1,135 @@
 import React, { useEffect, useState } from 'react'
-import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import { API_URL, API_KEY, IMAGE_BASE_URL,IMAGE_SIZE } from '../../Config';
 import MainImage from '../LandingPage/Sections/MainImage';
 import MovieInfo from './Sections/MovieInfo';
-import { Row, Button  } from 'antd';
+import {  List, Avatar, Row, Col, Button  } from 'antd';
 import GridCards from '../commons/GridCards';
 import Favorite from './Sections/Favorite';
+import Comments from './Sections/Comments'
+import LikeDislikes from './Sections/LikeDislikes';
+import axios from 'axios';
 
 function MovieDetail(props) {
 
-    let movieId = props.match.params.movieId;
-
+    const movieId = props.match.params.movieId
     const [Movie, setMovie] = useState([])
     const [Casts, setCasts] = useState([])
+    const [CommentLists, setCommentLists] = useState([])
+    const [LoadingForMovie, setLoadingForMovie] = useState(true)
+    const [LoadingForCasts, setLoadingForCasts] = useState(true)
     const [ActorToggle, setActorToggle] = useState(false)
+    const movieVariable = {
+        movieId: movieId
+    }
 
+    
     useEffect(() => {
-        
-        let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
-        
-        let endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
 
-        fetch(endpointInfo)
-        .then(response => response.json())
-        .then(response => {
-            setMovie(response)
-        })
+        let endpointForMovieInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`;
+        fetchDetailInfo(endpointForMovieInfo)
 
-        fetch(endpointCrew)
-        .then(response => response.json())
-        .then(response => {
-            setCasts(response.cast)
-        })
-        
+        axios.post('/api/comment/getComments', movieVariable)
+            .then(response => {
+                console.log(response)
+                if (response.data.success) {
+                    console.log('response.data.comments', response.data.comments)
+                    setCommentLists(response.data.comments)
+                } else {
+                    alert('Failed to get comments Info')
+                }
+            })
+
     }, [])
 
     const toggleActorView = () => {
         setActorToggle(!ActorToggle)
     }
 
+    const fetchDetailInfo = (endpoint) => {
+
+        fetch(endpoint)
+            .then(result => result.json())
+            .then(result => {
+                console.log(result)
+                setMovie(result)
+                setLoadingForMovie(false)
+
+                let endpointForCasts = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+                fetch(endpointForCasts)
+                    .then(result => result.json())
+                    .then(result => {
+                        console.log(result)
+                        setCasts(result.cast)
+                    })
+
+                setLoadingForCasts(false)
+            })
+            .catch(error => console.error('Error:', error)
+            )
+    }
+
+    const updateComment = (newComment) => {
+        setCommentLists(CommentLists.concat(newComment))
+    }
+
     return (
         <div>
+            {/* Header */}
+            {!LoadingForMovie ?
+                <MainImage
+                    image={`${IMAGE_BASE_URL}${IMAGE_SIZE}${Movie.backdrop_path}`}
+                    title={Movie.original_title}
+                    text={Movie.overview}
+                />
+                :
+                <div>loading...</div>
+            }
 
-            {/** Header */}
-            <MainImage 
-                image={`${IMAGE_BASE_URL}w1280${Movie.backdrop_path}`}
-                title={Movie.original_title}
-                text={Movie.overview}
 
-            />
+            {/* Body */}
+            <div style={{ width: '85%', margin: '1rem auto' }}>
 
-
-
-
-
-            {/** Body */}
-            <div style={{ width: '85%', margin: '1rem suto'}}>
-
-                <div style={{ display: 'flex', justifyContent: 'flex-end'}}>
-                    <Favorite  movieInfo={Movie} movieId={movieId} userFrom={localStorage.getItem('userId')} />
+                <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <Favorite movieInfo={Movie} movieId={movieId} userFrom={localStorage.getItem('userId')} />
                 </div>
 
-                {/** Movie Info */}
-                <MovieInfo 
-                    movie={Movie}
-                />
+
+                {/* Movie Info */}
+                {!LoadingForMovie ?
+                    <MovieInfo movie={Movie} />
+                    :
+                    <div>loading...</div>
+                }
+
+                <br />
+                {/* Actors Grid*/}
+
+                <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
+                    <Button onClick={toggleActorView}>Toggle Actor View </Button>
+                </div>
+
+                {ActorToggle &&
+                    <Row gutter={[16, 16]}>
+                        {
+                            !LoadingForCasts ? Casts.map((cast, index) => (
+                                cast.profile_path &&
+                                <GridCards
+                                    image={cast.profile_path ?
+                                        `${IMAGE_BASE_URL}w500${cast.profile_path}` : null}
+                                    characterName={cast.name}
+                                />)) :
+                                <div>loading...</div>
+                        }
+                    </Row>
+                }
                 <br />
 
-
-                {/** Actiors Grid  */}
-                <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
-                    <Button onClick={toggleActorView}> Toggle Actor View </Button>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <LikeDislikes video videoId={movieId} userId={localStorage.getItem('userId')} />
                 </div>
 
-                    {ActorToggle && 
-                        <Row gutter={[16, 16]} >
-
-                        {Casts && Casts.map((cast, index) => (
-                            <React.Fragment key={index}>
-                                <GridCards  
-                                image={cast.profile_path ?
-                                    `${IMAGE_BASE_URL}w500${cast.profile_path}` : null} 
-                                characterName={cast.name}
-                                />
-    
-                            </React.Fragment>
-                            ))}
-    
-                        </Row>
-                    
-                    }
-                    <br />
+                {/* Comments */}
+                <Comments movieTitle={Movie.original_title} CommentLists={CommentLists} postId={movieId} refreshFunction={updateComment} />
 
             </div>
 
